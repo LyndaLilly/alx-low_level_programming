615b098a3ac1840d09d1fce0ceb19e7c1f3cb453
Lynda <lilianlynda22@gmail.com>
2023-08-11 18:54:39 -0700

    Task 102-print_comb5.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,6 +14,7 @@ int main(void)
 		for (tens = '0'; tens <= '9'; tens++)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
+			{
 				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 				{
 					putchar(hundreds);
@@ -25,8 +26,9 @@ int main(void)
 						putchar(' ');
 					}
 				}
+			}
 		}
-	}
+	}	
 
 	putchar('\n');
 	return (0);
