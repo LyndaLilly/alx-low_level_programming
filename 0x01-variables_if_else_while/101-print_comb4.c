f646dc507f48d3818dedefed06a4e85d05346983
Lynda <lilianlynda22@gmail.com>
2023-08-11 19:46:36 -0700

    Task 100-print_comb3.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints all possible combinations of two digit
+ * main - prints all possible dif combinations of these digits
  * Return: Always 0
  */
 int main(void)
@@ -28,9 +28,11 @@ int main(void)
 				}
 			}
 		}
-	}	
-
+	}
 	putchar('\n');
 	return (0);
 }
+
+	
+
 
