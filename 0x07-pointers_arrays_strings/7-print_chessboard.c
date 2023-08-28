6a5a4ba899a00124581d13365afbfb2e2f52df6b
Lynda <lilianlynda22@gmail.com>
2023-08-28 14:19:39 -0700

    Task ./7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,26 @@
 #include "main.h"
+
 /**
- * print_chessboard - Entry point
- * @a: array
- * Return: Always 0 (Success)
+ * print-chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ *
+ * This function prints the chessboard by iterating through the 2D array
+ * of characters and printing each character to the standard output.
+ * Each row of the chessboard is separated by a newline character.
+ *
+ * @a: The 2D array representing the chessboard.
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		_putchar(a[i][j]);
+		{
+			_putchar(a[i][j]);
+		}
 		_putchar('\n');
 	}
 }
