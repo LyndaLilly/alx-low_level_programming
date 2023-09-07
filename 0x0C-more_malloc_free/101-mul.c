6d324222b8b9409fe91ed2787a60963b11472385
Lynda <lilianlynda22@gmail.com>
2023-09-07 08:09:59 -0700

    Task 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 #define ERR_MSG "Error"
 
 /**
@@ -43,7 +44,6 @@ int _strlen(char *s)
 /**
  * errors - handles errors for main
  */
-
 void errors(void)
 {
 	printf("Error\n");
@@ -57,13 +57,12 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-	s1 = argv[1], s2 = argv[2];
 
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
