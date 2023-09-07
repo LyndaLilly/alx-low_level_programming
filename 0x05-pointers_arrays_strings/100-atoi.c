3a9a808a7e12e254178b226595d9ed85c241e0d1
Lynda <lilianlynda22@gmail.com>
2023-09-07 04:54:59 -0700

    0x0C-more_malloc_free

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * _atoi - It converts a string to an integer
+ * @s: The string that will be converted
  *
  * Return: the int converted from the string
  */
