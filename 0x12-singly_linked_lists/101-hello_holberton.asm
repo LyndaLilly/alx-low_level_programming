adce3b673ca202d61f86f5353a82ce8cd858bde1
Lynda <lilianlynda22@gmail.com>
2023-09-22 08:29:25 -0700

    update task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    format db "Hello, Holberton",10,0  ; The format string with a newline character (10) and null terminator (0)
-
-section .text
-    global main
-
-extern printf  ; Declare the external printf function from the C library
-
+global    main
+extern    printf
 main:
-push rbp
-mov rdi, format  ; Set the format string as the first argument (printf expects it in rdi)
-call printf     ; Call the printf function
-add rsp, 8      ; Restore the stack pointer (adjust for the printf call)
-pop rbp
-; Exit the program
-mov rax, 60     ; System call number for exit (60 on x86_64)
-xor rdi, rdi    ; Exit status (0)
-syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
