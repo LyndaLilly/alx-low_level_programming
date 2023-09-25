c8ac2d2420947d0c94a991d38e5f7da354a8578a
Lynda <lilianlynda22@gmail.com>
2023-09-25 14:31:40 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "lists.h"
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *temp;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}
