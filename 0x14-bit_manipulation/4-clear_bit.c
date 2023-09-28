095871e0762239aa0acbd78370eee731ff7bc0e5
Lynda <lilianlynda22@gmail.com>
2023-09-28 13:49:09 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * clear_bit - sets the value of 0
+ * @n: pointer to no to chng
+ * @index: is the index, starting from 0 of the bit you want to set
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+	 if (index >= sizeof(unsigned long int) * 8)
+	 {
+		 return -1;
+	 }
+	 mask = ~(1UL << index);
+
+	*n = *n & mask;
+	 return 1;
+}
