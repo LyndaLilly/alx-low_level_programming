ac6810b6eac7046295bcc2ffe10141e5d26f6c88
Lynda <lilianlynda22@gmail.com>
2023-09-28 13:10:51 -0700

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,10 +9,11 @@
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1UL << index;
+
 	if (index >= sizeof(unsigned long int) * 8)
 	{
-		return -1;
+		return (-1);
 	}
 	*n |= mask;
-	return 1;
+	return (1);
 }
